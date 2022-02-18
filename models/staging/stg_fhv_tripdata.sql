e3bc0498d3d9eb21bdb345b7a7b4acd42c0f2f75
cloud.native.303@gmail.com <cloud.native.303@gmail.com>
2022-02-18 17:47:49 +0000

    fhv prob

--- models/staging/stg_fhv_tripdata.sql
+++ models/staging/stg_fhv_tripdata.sql
@@ -1,12 +1,28 @@
 with tripdata as 
 (
-  select *,
+  select 
+    dispatching_base_num,
+    pickup_datetime,
+    dropoff_datetime,
+    pulocationid,
+    dolocationid,
+    sr_flag,
     row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
   from {{ source('staging','fhv_tripdata') }}
   where dispatching_base_num is not null 
 )
 
-select *
+select 
+   -- identifiers
+    {{ dbt_utils.surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    dispatching_base_num as vendorid,
+
+        -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+    cast(pulocationid as integer) as  pickup_locationid,
+    cast(dolocationid as integer) as dropoff_locationid,
+    sr_flag
 from tripdata
 where rn = 1
 
