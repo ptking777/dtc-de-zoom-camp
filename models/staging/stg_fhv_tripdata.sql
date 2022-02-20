7e46003b22e75165ff4a99bc5fd5288460b21f85
cloud.native.303@gmail.com <cloud.native.303@gmail.com>
2022-02-20 03:32:06 +0000

    incompatible data types

--- models/staging/stg_fhv_tripdata.sql
+++ models/staging/stg_fhv_tripdata.sql
@@ -1,13 +1,13 @@
 with tripdata as 
 (
   select 
-    dispatching_base_num,
+    cast(substring(dispatching_base_num,2) as integer) as vendorid,,
     pickup_datetime,
     dropoff_datetime,
     --IFNULL(pulocationid,"0") AS pulocationid,
     --IFNULL(dolocationid,"0") AS dolocationid,
-    pulocationid,
-    dolocationid,
+    cast(pulocationid as integer) as pickup_locationid,
+    cast(dolocationid as integer) as dropoff_locationid,
     IFNULL(sr_flag,"0") AS sr_flag,
     row_number() over(partition by cast(substring(dispatching_base_num,2) as integer), pickup_datetime) as rn
   from {{ source('staging','fhv_tripdata') }}
@@ -20,13 +20,14 @@ with tripdata as
 select 
    -- identifiers
     {{ dbt_utils.surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
-    dispatching_base_num as vendorid,
+    vendorid,
+    cast(NULL as integer) as ratecodeid,
+    pickup_locationid,
+    dropoff_locationid,
 
         -- timestamps
     cast(pickup_datetime as timestamp) as pickup_datetime,
     cast(dropoff_datetime as timestamp) as dropoff_datetime,
-    cast(pulocationid as integer) as  pickup_locationid,
-    cast(dolocationid as integer) as dropoff_locationid,
     sr_flag
 from tripdata
 --where rn = 1
