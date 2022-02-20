c4d6d81b275d38f00eb19ed84ed3c61ed525cf1f
cloud.native.303@gmail.com <cloud.native.303@gmail.com>
2022-02-20 23:58:58 +0000

    safe_cast

--- models/staging/stg_fhv_tripdata.sql
+++ models/staging/stg_fhv_tripdata.sql
@@ -8,13 +8,13 @@ tripdata as (
     dispatching_base_num,
     pickup_datetime,
     dropoff_datetime,
-    cast(pulocationid as integer) as pickup_locationid,
-    cast(dolocationid as integer) as dropoff_locationid,
+    safe_cast(pulocationid as integer) as pickup_locationid,
+    safe_cast(dolocationid as integer) as dropoff_locationid,
     IFNULL(sr_flag,"0") AS sr_flag,
     row_number() over(partition by dispatching_base_num, pickup_datetime) as rn
   from {{ source('staging','fhv_tripdata') }}
   where dispatching_base_num is not null 
-  and pulocationid != 'PULocationID'
+  --and pulocationid != 'PULocationID'
 )
 
 
@@ -27,8 +27,8 @@ select
     dropoff_locationid,
 
         -- timestamps
-    cast(pickup_datetime as timestamp) as pickup_datetime,
-    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+    safe_cast(pickup_datetime as timestamp) as pickup_datetime,
+    safe_cast(dropoff_datetime as timestamp) as dropoff_datetime,
     sr_flag
 from tripdata
 inner join fhv_vendors
--- models/staging/stg_green_tripdata.sql
+++ models/staging/stg_green_tripdata.sql
@@ -8,33 +8,33 @@ with tripdata as
 select
     -- identifiers
     {{ dbt_utils.surrogate_key(['vendorid', 'lpep_pickup_datetime']) }} as tripid,
-    cast(vendorid as integer) as vendorid,
-    cast(ratecodeid as integer) as ratecodeid,
-    cast(pulocationid as integer) as  pickup_locationid,
-    cast(dolocationid as integer) as dropoff_locationid,
+    safe_cast(vendorid as integer) as vendorid,
+    safe_cast(ratecodeid as integer) as ratecodeid,
+    safe_cast(pulocationid as integer) as  pickup_locationid,
+    safe_cast(dolocationid as integer) as dropoff_locationid,
     
     -- timestamps
-    cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
-    cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
+    safe_cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
+    safe_cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
     
     -- trip info
     store_and_fwd_flag,
-    cast(passenger_count as integer) as passenger_count,
-    cast(trip_distance as numeric) as trip_distance,
-    cast(trip_type as integer) as trip_type,
+    safe_cast(passenger_count as integer) as passenger_count,
+    safe_cast(trip_distance as numeric) as trip_distance,
+    safe_cast(trip_type as integer) as trip_type,
     
     -- payment info
-    cast(fare_amount as numeric) as fare_amount,
-    cast(extra as numeric) as extra,
-    cast(mta_tax as numeric) as mta_tax,
-    cast(tip_amount as numeric) as tip_amount,
-    cast(tolls_amount as numeric) as tolls_amount,
-    cast(ehail_fee as numeric) as ehail_fee,
-    cast(improvement_surcharge as numeric) as improvement_surcharge,
-    cast(total_amount as numeric) as total_amount,
-    cast(payment_type as integer) as payment_type,
+    safe_cast(fare_amount as numeric) as fare_amount,
+    safe_cast(extra as numeric) as extra,
+    safe_cast(mta_tax as numeric) as mta_tax,
+    safe_cast(tip_amount as numeric) as tip_amount,
+    safe_cast(tolls_amount as numeric) as tolls_amount,
+    safe_cast(ehail_fee as numeric) as ehail_fee,
+    safe_cast(improvement_surcharge as numeric) as improvement_surcharge,
+    safe_cast(total_amount as numeric) as total_amount,
+    safe_cast(payment_type as integer) as payment_type,
     {{ get_payment_type_description('payment_type') }} as payment_type_description, 
-    cast(congestion_surcharge as numeric) as congestion_surcharge
+    safe_cast(congestion_surcharge as numeric) as congestion_surcharge
 from tripdata
 where rn = 1
 
